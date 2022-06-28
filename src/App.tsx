27359ea1556428e3247fc977b41b76882d6147fc
Ricardo Gava <ricardoagava@gmail.com>
2022-06-28 06:57:22 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <C.Container>
+        <C.Info>
+
+        </C.Info>
+        <C.GridArea>
+
+        </C.GridArea>
+      </C.Container>
+
     </div>
-  );
+  )
 }
 
 export default App;
