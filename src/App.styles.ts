8a43df005625db7811bd86d37034f34d432f30df
Ricardo Gava <ricardoagava@gmail.com>
2022-07-12 04:10:16 -0300

    All styled components are positioned including responsivity

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,11 +1,50 @@
 import styled from "styled-components"
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100%;
+  max-width: 750px;
+  margin: auto;
+  display: flex;
+  padding: 50px 0;
 
-export const Info = styled.div``
+  @media (max-width: 750px) {
+    flex-direction: column;
+  }
+`
 
-export const LogoLink = styled.a``
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: auto;
 
-export const InfoArea = styled.div``
+  @media (max-width: 750px) {
+    margin-bottom: 50px;
+    align-items: center;
+  }
+`
 
-export const GridArea = styled.div``
+export const LogoLink = styled.a`
+  display: block;
+`
+
+export const InfoArea = styled.div`
+  width: 100%;
+  margin: 10px 0;
+
+  @media (max-width: 750px) {
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+  }
+`
+
+export const GridArea = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+
+  @media (max-width: 750px) {
+    justify-content: center;
+    margin: 0 20px;
+  }
+`
