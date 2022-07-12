cd8e012b7b6454e85c47d8cd63876faa45c4b4e2
Ricardo Gava <ricardoagava@gmail.com>
2022-07-12 19:32:31 -0300

    Created a button component with an optional icon

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import * as C from './styles'
+
+type Props = {
+  label: string
+  icon?: string
+  onClick: React.MouseEventHandler<HTMLButtonElement>
+}
+
+export const Button = ({ label, icon, onClick }: Props) => {
+  return (
+    <C.Container onClick={onClick}>
+      {icon &&
+        <C.IconArea>
+          <C.Icon src={icon} />
+        </C.IconArea>
+      }
+      <C.Label>{label}</C.Label>
+    </C.Container>
+  )
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  width: 200px;
+  height: 50px;
+  display: flex;
+  background-color: #1550FF;
+  border: none;
+  border-radius: 10px;
+  opacity: 1;
+  transition: all ease .3s;
+
+  &:hover {
+    opacity: .8;
+  }
+`
+
+export const IconArea = styled.div`
+  height: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-right: 1px solid rgba(255, 255, 255, .2);
+  padding: 0 15px;
+`
+
+export const Icon = styled.img`
+  height: 20px;
+`
+
+export const Label = styled.span`
+  height: inherit;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  padding: 0 20px;
+`
