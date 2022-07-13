8f988332c1b90b197db3d645534a3d3b8f4d7a5c
Ricardo Gava <ricardoagava@gmail.com>
2022-07-13 03:09:37 -0300

    Created the card elements with a name and an icon property

---
+++ src/utils/cards.ts
@@ -0,0 +1,15 @@
+import androidSvg from '../svgs/android.svg'
+import caminhao from '../svgs/caminhao.svg'
+import disney from '../svgs/disney.svg'
+import estrela from '../svgs/estrela.svg'
+import gasolina from '../svgs/gasolina.svg'
+import moto from '../svgs/moto.svg'
+
+export const cards = [
+  { name: 'android', icon: androidSvg },
+  { name: 'caminhao', icon: caminhao },
+  { name: 'disney', icon: disney },
+  { name: 'estrela', icon: estrela },
+  { name: 'gasolina', icon: gasolina },
+  { name: 'moto', icon: moto },
+]
