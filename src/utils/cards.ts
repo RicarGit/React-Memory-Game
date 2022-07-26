2b853e162e65f6a0ffb172cafdc867686c3885eb
Ricardo Gava <ricardoagava@gmail.com>
2022-07-25 22:45:53 -0300

    Modified all the svg images and adding more images also increasing the number of cards to play

--- src/utils/cards.ts
+++ src/utils/cards.ts
@@ -1,15 +1,23 @@
-import androidSvg from '../svgs/android.svg'
-import caminhao from '../svgs/caminhao.svg'
-import disney from '../svgs/disney.svg'
-import estrela from '../svgs/estrela.svg'
-import gasolina from '../svgs/gasolina.svg'
-import moto from '../svgs/moto.svg'
+import chocolate from '../svgs/chocolate.svg'
+import cocktail from '../svgs/cocktail.svg'
+import coffee from '../svgs/coffee.svg'
+import cookie from '../svgs/cookie.svg'
+import donut from '../svgs/donut.svg'
+import forkAndKnife from '../svgs/fork-and-knife.svg'
+import frenchFries from '../svgs/french-fries.svg'
+import hamburger from '../svgs/hamburger.svg'
+import iceCream from '../svgs/ice-cream.svg'
+import pudding from '../svgs/pudding.svg'
 
 export const cards = [
-  { name: 'android', icon: androidSvg },
-  { name: 'caminhao', icon: caminhao },
-  { name: 'disney', icon: disney },
-  { name: 'estrela', icon: estrela },
-  { name: 'gasolina', icon: gasolina },
-  { name: 'moto', icon: moto },
+  { name: 'chocolate', icon: chocolate },
+  { name: 'cocktail', icon: cocktail },
+  { name: 'coffee', icon: coffee },
+  { name: 'cookie', icon: cookie },
+  { name: 'donut', icon: donut },
+  { name: 'forkAndKnife', icon: forkAndKnife },
+  { name: 'frenchFries', icon: frenchFries },
+  { name: 'hamburger', icon: hamburger },
+  { name: 'iceCream', icon: iceCream },
+  { name: 'pudding', icon: pudding },
 ]
