b66c220abdc06e61214db9e983bfbec87cdcf5e2
Ricardo Gava <ricardoagava@gmail.com>
2022-07-30 08:39:00 -0300

    Created a root component and created a reset gobal style from styled-components

---
+++ src/Root.tsx
@@ -0,0 +1,29 @@
+import App from "./App";
+
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import { theme } from 'config/themes/theme'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`
+
+export const Root = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
+import { Root } from './Root'
+
+const root = createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Root />
+  </StrictMode>
 )
